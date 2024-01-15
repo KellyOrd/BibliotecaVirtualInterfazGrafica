f49a8babb1a3be35717ea9f830e4328e02ce5949
KellyOrd <147111436+KellyOrd@users.noreply.github.com>
2024-01-15 06:00:16 -0500

    Add files via upload

---
+++ IBibliotecaDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.poo.ejemploguiapp.idao;
+
+import ec.edu.ups.poo.ejemplouiapp.modelo.Biblioteca;
+import java.util.List;
+
+public interface IBibliotecaDAO {
+    void crearBiblioteca(Biblioteca biblioteca);
+    Biblioteca obtenerBiblioteca(int codigo);
+    void actualizarBiblioteca(int codigo, Biblioteca biblioteca);
+    void eliminarBiblioteca(int codigo);
+    List<Biblioteca> listarBiblioteca();
+}
---
+++ IPrestamoDAO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.poo.ejemploguiapp.idao;
+
+import ec.edu.ups.poo.ejemplouiapp.modelo.Prestamo;
+import java.util.List;
+
+
+public interface IPrestamoDAO {
+    List<Prestamo> listarPrestamo();
+    Prestamo obtenerPrestamo(int id);
+    void crearPrestamo(Prestamo prestamo);
+    void actualizarPrestamo(int id, Prestamo prestamo);
+    void eliminarPrestamo(int id);
+}
